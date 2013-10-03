66a43c9ca4336e4dd8bbd2a1d635bea222d3a262
Ludovic <ludo044@gmail.com>
2013-10-04 00:47:27 +0200

    Début d'implémentation des DAO

---
+++ src/main/java/net/leludo/gtrchamp/dao/ChampionnatDao.java
@@ -0,0 +1,15 @@
+package net.leludo.gtrchamp.dao;
+
+import javax.persistence.EntityManagerFactory;
+
+public class ChampionnatDao {
+	private EntityManagerFactory emf;
+	
+	public ChampionnatDao() {
+		
+	}
+	
+	public void find(int id) {
+		
+	}
+}
