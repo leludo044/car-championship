d9f1152d954793af5b8f6183ecdb687600895116
leludo044@gmail.com <leludo044@gmail.com>
2013-11-21 08:49:52 +0100

    Adaptation au schéma de SQLite

--- src/main/java/net/leludo/gtrchamp/Championnat.java
+++ src/main/java/net/leludo/gtrchamp/Championnat.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "championnat")
+@Table(name = "championnats")
 public class Championnat {
 	@Id
 	private int id;
