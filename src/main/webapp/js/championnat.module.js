a94cb1393a2cd2b098ad3f3ff329b4b72b87de0c
leludo044@gmail.com <leludo044@gmail.com>
2013-12-20 10:52:15 +0100

    Externalisation du code Angular.js

---
+++ src/main/webapp/js/championnat.module.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+var chpApp = angular.module('chpApp', []);
+
+chpApp.controller('ChampionnatController', function($scope, $http) {
+	// Initialising the variable.
+	$scope.championnat = [ {
+		"id" : 1,
+		"libelle" : "Championnat 2012"
+	} ];
+
+	// Getting the list of users through ajax call.
+	$http({
+		url : 'http://localhost:8080/gtrchamp2/ws/championnat/getjson/1',
+		method : 'GET',
+	}).success(function(data, status, headers, config) {
+		$scope.championnat = data;
+	}).error(function(data, status, headers, config) {
+		// called asynchronously if an error occurs
+		// or server returns response with an error status.
+		$scope.championnat = [ {
+			"id" : 1,
+			"libelle" : "Championnat 2012"
+		} ];
+	});
+});
