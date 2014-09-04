7e06d3fc6a7167677410b2e8053d7fa5f836c5a9
Ludovic <leludo044@gmail.com>
2014-09-04 16:49:32 +0200

    Stats sur le nombre de victoires

--- src/main/webapp/app/directives/stats.js
+++ src/main/webapp/app/directives/stats.js
@@ -1,7 +1,67 @@
 chpApp.directive('statsVictories', function() {
   return {
-      restrict: 'AE',
+      restrict: 'E',
       replace: 'true',
-      templateUrl: 'app/partials/tableVictories.html'
+      template: '<div id="chart">tot</div>',
+      link: function (scope, element, attrs) {
+          console.log(3);
+          var ideas = [
+                          ['ideas1', 1],
+                          ['ideas2', 8],
+                          ['ideas3', 5]
+                        ];
+          var chart = new Highcharts.Chart({
+            chart: {
+              renderTo: 'chart',
+              type: 'column'
+            },
+            title: {
+              text: 'Victoires'
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },            
+            xAxis: {
+                categories: [
+                    'Jan',
+                    'Feb',
+                    'Mar',
+                    'Apr',
+                    'May',
+                    'Jun',
+                    'Jul',
+                    'Aug',
+                    'Sep',
+                    'Oct',
+                    'Nov',
+                    'Dec'
+                ]
+            },
+            yAxis: {
+                min: 0
+            },
+            series: [{
+                name: 'Tokyo',
+                data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+            }, {
+                name: 'New York',
+                data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+            }, {
+                name: 'London',
+                data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+
+            }, {
+                name: 'Berlin',
+                data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+
+            }]
+          });
+          
+        }
   };
 });
