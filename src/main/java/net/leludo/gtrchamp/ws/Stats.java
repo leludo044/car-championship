9e7b1b9f567b817b8c45656da9df714c076c9458
Ludovic <leludo044@gmail.com>
2015-02-23 22:28:24 +0100

    Suppression des warnings Java

--- src/main/java/net/leludo/gtrchamp/ws/Stats.java
+++ src/main/java/net/leludo/gtrchamp/ws/Stats.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManagerFactory;
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
