d064263ad247d4bcdf94901b6e1a5ab0e68d9cf7
leludo044 <leludo044@gmail.com>
2015-11-27 17:21:38 +0100

    Commentaires sur le BO statistique

--- src/main/java/net/leludo/gtrchamp/stat/Stat.java
+++ src/main/java/net/leludo/gtrchamp/stat/Stat.java
@@ -1,19 +1,47 @@
 package net.leludo.gtrchamp.stat;
 
+/**
+ * Représente une données statistiques pour un pilote
+ */
 public class Stat {
 
 	public String name;
 	public Integer count;
-	
+
+	/**
+	 * Retourne le nom du pilote
+	 * 
+	 * @return Le nom du pilote
+	 */
 	public String getName() {
 		return name;
 	}
+
+	/**
+	 * Fixe le nom du pilote
+	 * 
+	 * @param name
+	 *            Le nouveau nom du pilote
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	/**
+	 * Retourne la valeur statistique
+	 * 
+	 * @return La valeur statistique
+	 */
 	public Integer getCount() {
 		return count;
 	}
+
+	/**
+	 * Fixe la valeur statistique
+	 * 
+	 * @param count
+	 *            La nouvelle valeur statistique
+	 */
 	public void setCount(Integer count) {
 		this.count = count;
 	}
