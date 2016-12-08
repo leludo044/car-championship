b22943116feab12426f4310b0c8e09864cb9dc05
leludo044 <leludo044@gmail.com>
2016-12-08 17:31:08 +0100

    Improve country web service tests

--- src/test/java/net/leludo/gtrchamp/ws/CountryWebServiceTest.java
+++ src/test/java/net/leludo/gtrchamp/ws/CountryWebServiceTest.java
@@ -1,72 +1,63 @@
 package net.leludo.gtrchamp.ws;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 
-import net.leludo.gtrchamp.dao.CountryDao;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-//@RunWith(MockitoJUnitRunner.class)
-public class CountryWebServiceTest extends JerseyTest  {
-    @Mock
-    private CountryDao dao;
-    @InjectMocks
+import net.leludo.connector.BdConnector;
+import net.leludo.gtrchamp.Country;
+import net.leludo.gtrchamp.dao.DaoFactory;
+
+public class CountryWebServiceTest extends JerseyTest {
+
+    DaoFactory daoFactory;
+
+    // @Mock
+    // private CountryDao dao;
+
+    // @InjectMocks
     private CountryWebService countryWebService;
 
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-    }
+//    @Before
+//    public void setUp() throws Exception {
+//        super.setUp();
+//    }    
     
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Override
     protected Application configure() {
-//        /return new ResourceConfig(CountryWebService.class);
-        return new ResourceConfig(HelloResource.class);
+
+        init();
+        /// daoFactory = Mockito.spy(DaoFactory.class);
+        // Mockito.when(daoFactory.countryDao()).thenReturn(dao);
+
+        return new ResourceConfig(CountryWebService.class);
+        // return new ResourceConfig(HelloResource.class);
 
     }
-    
-    @Path("hello")
-    public static class HelloResource {
-        @GET
-        public String getHello() {
-            return "Hello World!";
-        }
-    }
-    
-    @Test
-    public void test() {
-        final String hello = target("hello").request().get(String.class);
-        assertEquals("Hello World!", hello);
-    }
+
     @Test
     public void testCountries() throws Exception {
-        Response response = target("/country").request().get();
+        String response = target("/country").request().get(String.class);
         assertNotNull(response);
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<Country> countries = mapper.readValue(response, List.class);
+        System.out.println(countries);
+        assertTrue(countries.size() > 0);
     }
 
     @Test
@@ -89,4 +80,31 @@ public class CountryWebServiceTest extends JerseyTest  {
         throw new RuntimeException("not yet implemented");
     }
 
+    private void init() {
+        EntityManagerFactory emf;
+        String uri = System.getenv("CLEARDB_DATABASE_URL");
+        if (uri != null) {
+            try {
+                URI dbUri;
+                dbUri = new URI(uri);
+
+                BdConnector connector = new BdConnector(dbUri);
+
+                Properties properties = new Properties();
+                properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+                properties.put("hibernate.connection.url", connector.getUrl());
+                properties.put("hibernate.connection.username", connector.getUsername());
+                properties.put("hibernate.connection.password", connector.getPassword());
+                properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+                properties.put("hibernate.show_sql", "false");
+
+                emf = Persistence.createEntityManagerFactory("gtrchamp", properties);
+
+                DaoFactory.getInstance().setEntityManagerfactory(emf);
+            } catch (URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 }
