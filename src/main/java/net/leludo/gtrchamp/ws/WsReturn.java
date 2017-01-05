d9d4ac349cf433d23340ccd62614b203c6d88d10
Ludovic <leludo044@gmail.com>
2017-01-05 22:29:19 +0100

    Comments response wrapper for web service

--- src/main/java/net/leludo/gtrchamp/ws/WsReturn.java
+++ src/main/java/net/leludo/gtrchamp/ws/WsReturn.java
@@ -1,32 +1,49 @@
 package net.leludo.gtrchamp.ws;
 
+/**
+ * Wrapper for web service response
+ */
 public class WsReturn {
 
-    private int code;
-    private String message;
+	private int code;
+	private String message;
 
-    public WsReturn() {
-        this.code = 0;
-        this.message = "";
-    }
+	/**
+	 * Constructor with default response code to 0 and empty response message
+	 */
+	public WsReturn() {
+		this.code = 0;
+		this.message = "";
+	}
 
-    public WsReturn(final int code, final String message) {
-        this.code = code;
-        this.message = message;
-    }
+	/**
+	 * Constructor
+	 * 
+	 * @param code
+	 *            Response code
+	 * @param message
+	 *            Response message
+	 */
+	public WsReturn(final int code, final String message) {
+		this.code = code;
+		this.message = message;
+	}
 
-    /**
-     * @return the code
-     */
-    public int getCode() {
-        return code;
-    }
+	/**
+	 * Return the response code
+	 * 
+	 * @return The response code
+	 */
+	public int getCode() {
+		return code;
+	}
 
-    /**
-     * @return the message
-     */
-    public String getMessage() {
-        return message;
-    }
-
+	/**
+	 * Return the response message
+	 * 
+	 * @return The response message
+	 */
+	public String getMessage() {
+		return message;
+	}
 }
