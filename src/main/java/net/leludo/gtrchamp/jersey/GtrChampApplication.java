716fd9a71876d3c08b483579aa80cb43349eb90d
Ludovic <leludo044@gmail.com>
2017-01-05 21:24:00 +0100

    Comments Jersey classes

--- src/main/java/net/leludo/gtrchamp/jersey/GtrChampApplication.java
+++ src/main/java/net/leludo/gtrchamp/jersey/GtrChampApplication.java
@@ -5,9 +5,12 @@ import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
+/**
+ * Configuration of the Jersey web service controllers
+ */
 @ApplicationPath("ws")
 public class GtrChampApplication extends ResourceConfig {
-    public GtrChampApplication() {
-        packages("net.leludo.gtrchamp.ws").register(JacksonFeature.class);
-    }
+	public GtrChampApplication() {
+		packages("net.leludo.gtrchamp.ws").register(JacksonFeature.class);
+	}
 }
