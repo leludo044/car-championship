3af73ce310dbc1708b5aff840a0d645910d229df
Ludovic <leludo044@gmail.com>
2018-03-16 15:03:38 +0100

    Register each Jersey web service beacause use of ResouceConfig.packages() function doesn't work.

--- src/main/java/net/leludo/gtrchamp/jersey/GtrChampApplication.java
+++ src/main/java/net/leludo/gtrchamp/jersey/GtrChampApplication.java
@@ -6,6 +6,12 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import net.leludo.gtrchamp.ws.ChampionshipWebService;
+import net.leludo.gtrchamp.ws.CountryWebService;
+import net.leludo.gtrchamp.ws.DriverWebService;
+import net.leludo.gtrchamp.ws.RaceWebService;
+import net.leludo.gtrchamp.ws.TrackWebService;
+
 /**
  * Configuration of the Jersey web service controllers.
  */
@@ -16,7 +22,13 @@ public class GtrChampApplication extends ResourceConfig {
      * Constructor.
      */
     public GtrChampApplication() {
-        packages("net.leludo.gtrchamp.ws").register(JacksonFeature.class)
-                .register(CORSResponseFilter.class);
+        /*
+         * Need registration of each web service because
+         * packages("net.leludo.gtrchamp.ws") doesn't work
+         */
+        register(ChampionshipWebService.class)
+                .register(CountryWebService.class).register(DriverWebService.class)
+                .register(RaceWebService.class).register(TrackWebService.class)
+                .register(JacksonFeature.class).register(CORSResponseFilter.class);
     }
 }
